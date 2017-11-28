981a62d1b30ec78c34813f13c48e19c6b580ed89
sunlitkid <499954541@qq.com>
2017-11-28 15:33:29 +0800

    1

--- src/main/java/_9_Palindrome_Number.java
+++ src/main/java/_9_Palindrome_Number.java
@@ -6,6 +6,9 @@
  * Determine whether an integer is a palindrome. Do this without extra space.
  */
 public class _9_Palindrome_Number {
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(1000021));
+    }
     public static boolean isPalindrome(int x) {
         if (x<0 || (x!=0 && x%10==0)) return false;
         int rev = 0;
@@ -14,5 +17,22 @@ public class _9_Palindrome_Number {
             x = x/10;
         }
         return (x==rev || x==rev/10);
+
+    }
+
+    ///ERROR  System.out.println(isPalindrome2(1000021));
+    public static boolean isPalindrome2(int x) {
+        if(x<0){
+            return  false;
+        }
+        while(x>9){
+            int y = (int)(x / Math.pow(10,(int)Math.floor(Math.log10(x))));
+            if(y!=x%10){
+                return  false;
+            }
+            x = x%(int)Math.pow(10, (int)Math.floor(Math.log10(x)));
+            x=  x/10;
+        }
+        return  true;
     }
 }
